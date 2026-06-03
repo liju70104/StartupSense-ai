65566f91a955122c044b68f1e20d0dc8455c400a
Liju <liju70104@gmail.com>
2026-06-03 17:18:38 +0530

    Prepare project for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,18 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-MONGO_URL = "mongodb://localhost:27017"
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
 
 client = MongoClient(MONGO_URL)
 
 db = client["startup_sense_ai"]
 
 users = db["users"]
-
 startup_ideas = db["startup_ideas"]
-
 analysis_results = db["analysis_results"]
+reports = db["reports"]
 
 print("MongoDB Connected Successfully")
