d9cbb249c52c4411e57eefc98f572c6ffd3147a2
Liju <liju70104@gmail.com>
2026-06-19 19:41:44 +0530

    Fix app syntax and layout

--- frontend/app.py
+++ frontend/app.py
@@ -10,7 +10,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-)
 
 with open("frontend/assets/style.css", encoding="utf-8") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
